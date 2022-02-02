8f6f519bf0b545de3114d5f7b822fce822d578e9
Mareks Misukovs <98460992+ShtiroonsMareks@users.noreply.github.com>
2022-02-02 11:52:07 +0000

    2.uzd izpildīts

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
+"""
+1.uzd 
 diag=float(input("Ievadi kvadrāta diagonāles gaumu (cm): "))
 def laukums(x):
   mala_a= x/math.sqrt(2)
@@ -6,3 +8,25 @@ def laukums(x):
   return laukums_kv
 rezultats=laukums(diag)
 print(f"Kvadrāta laukums ={rezultats} cm^2")
+
+
+
+2.uzd
+
+a1=int(input("Ievadi 1.skaitli: "))
+a2=int(input("Ievadi 2.skaitli: "))
+
+elements=int(input("Ievadi n elementu: "))
+
+def virkne(x,y,n):
+  li=[1,7]
+  for i in range(1,n-1):
+    g=li[i-1]+li[i]
+    h=g%10
+    li.append(h)
+  rez = li[n-1]
+  return rez
+rezultats = virkne(a1,a2,elements)
+print(rezultats)
+"""
+
