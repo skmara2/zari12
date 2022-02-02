2478b14ebd5f033119c6d874fe71e0d3e68e2a82
Edvards Janitis <98461113+edavardis@users.noreply.github.com>
2022-02-02 12:27:08 +0000

    Pabeidzu kodu

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import math
 import random
 
+a1=int(input("Ievadi virknes 1. skaitli: ")) #1
+a2=int(input("Ievadi virknes 2. skaitli: ")) #7
+ntais=int(input("Ievadi n elementu: ")) #12
+
+def virkne(a,b,n):
+  sk=[a,b]
+  for i in range(1, n-1):
+    p=sk[i-1]+sk[i]
+    o=p%10
+    sk.append(o)
+  rez=sk[n-1]
+  return rez
+rezultats=virkne(a1,a2,ntais)
+print(rezultats)
